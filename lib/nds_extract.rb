4d3fd2e7b5a02cd309d66cea0d0c1519e2622a19
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-28 01:18:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -41,24 +41,6 @@ def movies_with_director_key(name, movies_collection) #iterate over movies colle
   while row_index < movies_collection.length do 
   new_array << movie_with_director_name(name, movies_collection[row_index]) 
   row_index += 1 
-    
-    
-   
-  
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
 end
 return new_array
 end
